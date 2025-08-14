4d8770dee1836356817509674c0f2def2ac421be
Mason D Young <masondyoung@gmail.com>
2025-08-13 18:37:39 -0700

    changes

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const API_KEY = 'AIzaSyBPPpFZK-Ol4Ta4-YKu95avsfDrJikEWRw'; // replace with your API key
   const SHEET_ID = '1dzuDIhVH3zqsIoEtgikrDQLFxK3oTxtxzBS_0qsXw6Y'; // replace with your sheet ID
   const RANGE = 'Test!A1:B1000000'; // replace with your data range
-  const POLL_INTERVAL = 5000; // Polling interval in milliseconds (e.g., 60000 ms = 1 minute)
+  const POLL_INTERVAL = 5000; // Polling interval in milliseconds
 
   let dataCache = [];
   let reversed = false;
+
+  // Check if the URL contains ?flip or ?flip=true
+  const params = new URLSearchParams(window.location.search);
+  if (params.has('flip')) {
+    reversed = true;
+  }
 
   const fetchSheetData = async () => {
     const url = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${RANGE}?key=${API_KEY}&t=${new Date().getTime()}`;
@@ -13,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const response = await fetch(url);
       if (response.ok) {
         const data = await response.json();
-        return data.values; // returns the data in a 2D array
+        return data.values;
       } else {
         console.error('Error fetching data from Google Sheets:', response.statusText);
         return [];
@@ -34,10 +40,7 @@ document.addEventListener('DOMContentLoaded', () => {
       return;
     }
 
-    // Set table headers
     headers.innerHTML = data[0].map(header => `<th>${header}</th>`).join('');
-
-    // Set table body
     body.innerHTML = data.slice(1).map(row => {
       return `<tr>${row.map(cell => `<td>${cell}</td>`).join('')}</tr>`;
     }).join('');
@@ -56,13 +59,7 @@ document.addEventListener('DOMContentLoaded', () => {
     renderTable(reversed ? dataCache.slice().reverse() : dataCache);
   };
 
-  // Load data initially
   loadData();
-
-  // Set up polling
   setInterval(loadData, POLL_INTERVAL);
-
-  // Add event listener to toggle button
   document.getElementById('toggle-order').addEventListener('click', toggleOrder);
 });
-
